0daf6a52296ecc9b1db0330cf22dda44a6db3937
joesev2022 <jose.apazasev@gmail.com>
2022-06-20 23:37:00 -0500

    Primer commit

---
+++ src/main/java/com/jaserviciosweb/microservicedollaranalyzer/MicroserviceDollarAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.jaserviciosweb.microservicedollaranalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDollarAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDollarAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaserviciosweb/microservicedollaranalyzer/MicroserviceDollarAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaserviciosweb.microservicedollaranalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDollarAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
